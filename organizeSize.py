35c81fff7639ebbd81e8ce33ad1c28c4eb5c7bb7
clairezqr <145880903+clairezqr@users.noreply.github.com>
2023-09-23 21:18:23 -0400

    Create organizeSize.py
    Python_1

---
+++ organizeSize.py
@@ -0,0 +1,42 @@
+def organizeSize():
+	for j in range(3):
+		box[j] = int(box[j])
+	box.sort()
+	volume = box[0] * box[1] * box[2]
+	return volume
+
+n = int(input())
+sizes = {}
+for i in range(n):
+	box = input().split()
+	volume = organizeSize() 
+	if volume in sizes:
+		sizes[volume].append(box)
+	else:
+		sizes[volume] = [box]
+
+all_volumes = sorted(sizes.keys())
+
+m = int(input())
+for i in range(m):
+	box = input().split()
+	volume = organizeSize()
+	start = -1
+	for j in range(len(all_volumes)):
+		if all_volumes[j] >= volume:
+			start = j
+			break
+	if start == -1:
+		print('Item does not fit.')
+	else:
+		check = False
+		for j in all_volumes[start:]:
+			if check:
+				break
+			for k in sizes[j]:
+				if k[0] >= box[0] and k[1] >= box[1] and k[2] >= box[2]:
+					print(j)
+					check = True
+					break
+		if check == False:
+			print('Item does not fit.')
